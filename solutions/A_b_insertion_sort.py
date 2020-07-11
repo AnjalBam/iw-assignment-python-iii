995f62404a8c9b3f0ef1db90ae39a7d360d81841
AnjalBam <anjalbam81@gmail.com>
2020-07-11 17:25:54 +0545

    Insertion method completed

---
+++ solutions/A_b_insertion_sort.py
@@ -0,0 +1,28 @@
+"""
+A. Make pythonic solutions for each of the following data structure
+and algorithm problems.
+a) Insertion sort
+"""
+lst = [12, 8, 6, 14, 9, 5, 1, 6]
+
+
+def sort_by_insertion_method(sample_list, reverse=False):
+    len_of_list = len(sample_list)
+    for i in range(len_of_list):
+        ref_num = sample_list[i]
+
+        j = i - 1
+        if reverse:
+            while j >= 0 and sample_list[j] < ref_num:
+                sample_list[j + 1] = sample_list[j]
+                j -= 1
+        else:
+            while j >= 0 and sample_list[j] > ref_num:
+                sample_list[j + 1] = sample_list[j]
+                j -= 1
+
+        sample_list[j + 1] = ref_num
+
+
+sort_by_insertion_method(lst, reverse=True)
+print(lst)
